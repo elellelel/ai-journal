ceff5f3f633fd40764719032f10a91f68e062e4b
Hazel Randquist <hrandquist@nc271814.lan>
2025-01-13 09:16:51 -0500

    Playing with versions yay

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,21 +1,22 @@
 const { environment } = require('@rails/webpacker')
-//const { VueLoaderPlugin } = require('vue-loader')
-//const vue = require('./loaders/vue')
 
-//environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-//environment.loaders.prepend('vue', {
-//    test: /\.vue$/,
-//    use: [{
-//        loader: 'vue-loader'
-//    }]
-//})
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
-//environment.loaders.append('babel', {
-//  test: /\.js$/,
-//  exclude: /node_modules/,
-//  use: {
-//    loader: 'babel-loader',
-//  },
-//});
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
+
+environment.loaders.append('babel', {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+  },
+});
 
 module.exports = environment
