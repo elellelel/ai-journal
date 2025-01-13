72210a2182d966971cc93cf3de4a8cd3f35ba49f
Hazel Randquist <hrandquist@nc271814.lan>
2025-01-13 08:45:00 -0500

    Versions should be compatible now

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,21 +1,21 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+//const { VueLoaderPlugin } = require('vue-loader')
+//const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', {
-    test: /\.vue$/,
-    use: [{
-        loader: 'vue-loader'
-    }]
-})
+//environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+//environment.loaders.prepend('vue', {
+//    test: /\.vue$/,
+//    use: [{
+//        loader: 'vue-loader'
+//    }]
+//})
 
-environment.loaders.append('babel', {
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-  },
-});
+//environment.loaders.append('babel', {
+//  test: /\.js$/,
+//  exclude: /node_modules/,
+//  use: {
+//    loader: 'babel-loader',
+//  },
+//});
 
 module.exports = environment
