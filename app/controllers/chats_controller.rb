72d567fc014851e679b7813b7ac962698d67ff05
Hazel Randquist <hrandquist@nc271814.lan>
2025-01-14 15:50:48 -0500

    Fixes content generation

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,7 +3,7 @@ class ChatsController < ApplicationController
     entry_ids = params[:chat][:linkedEntryIds]
     content = EntryFeedCreator.new(entry_ids).create_feed
 
-    response = OpenaiService.generate_response(content)
+    response = OpenaiService.generate_response(params[:message] + content)
 
     paragraphs = response.split("\n").reject(&:blank?)
     response = paragraphs.map { |p| "<p>#{p.strip}</p>" }.join
