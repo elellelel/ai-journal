748893e779d1ea072cf02638ea1a605d99f4d56a
Hazel Randquist <hrandquist@nc271814.lan>
2025-01-15 04:39:27 -0500

    Adds fullscreen option to TinyMCE

--- app/javascript/components/WritingCenter.vue
+++ app/javascript/components/WritingCenter.vue
@@ -7,7 +7,10 @@ import Editor from '@tinymce/tinymce-vue'
     <Editor
       api-key="k2v84nlvkh8npo92qtm0bne99sm2jg4s8a2776jajajodn3w"
       :init="{
-        plugins: 'lists link image table code help wordcount'
+        plugins: 'fullscreen lists link image table code help wordcount',
+        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | fullscreen', // Add fullscreen to the toolbar
+        height: 500, // Set a default height
+        menubar: 'file edit view insert format tools table help' // Include menu for better control
       }"
     />
   </main>
